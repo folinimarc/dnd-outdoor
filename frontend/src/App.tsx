7d929dc21494c7c8d97cebe4fc897fb6c0e1d129
ChlineSaurus <95419378+ChlineSaurus@users.noreply.github.com>
2025-03-16 12:49:49 +0100

    feat: setup frontend with TS, React and Vite

---
+++ frontend/src/App.tsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Dnd Outdoor go brrrr...</h1>
+      <div className="card">
+        <p>
+          Huiuiuiui...
+        </p>
+      </div>
+    </>
+  )
+}
+
+export default App
